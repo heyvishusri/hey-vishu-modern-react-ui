24bf5ddd068b8f8fedf1ae2f9589b4801fd0cf93
heyvishusri <vishwaskumar55347@gmail.com>
2025-07-10 01:09:55 +0530

    Navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import React from "react";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
 }
+
+export default App;
