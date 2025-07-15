55dd0f9d02c5c406e0fd719c0dd4574d4d7079ab
heyvishusri <vishwaskumar55347@gmail.com>
2025-07-16 02:39:19 +0530

    Updated css and app file

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import ScheduleSection from "./components/ScheduleSection";
 import MonitorSection from "./components/MonitorSection";
 import PricingSection from "./components/PricingSection";
 import ServicesSection from "./components/ServicesSection";
+import TestimonialSection from "./components/TestimonialSection";
+import NewsletterSection from "./components/NewsLetterSection";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -23,6 +26,9 @@ function App() {
         <MonitorSection />
         <PricingSection />
         <ServicesSection />
+        <TestimonialSection />
+        <NewsletterSection />
+        <Footer />
       </div>
     </main>
   );
